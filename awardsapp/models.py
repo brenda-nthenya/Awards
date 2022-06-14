1bd9d6be7d570abb3ab49ed23b9549aa77ba1b52
Brenda <bee@bee.com>
2022-06-14 11:15:50 +0300

    Creates a create_user_profile function

--- awardsapp/models.py
+++ awardsapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
 from django.utils import timezone
 from django.db.models.signals import post_save
 from PIL import Image
@@ -15,3 +16,8 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
