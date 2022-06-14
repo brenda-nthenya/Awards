4d489f91ee2c01ef80a6438b3ec3f85d09ab5751
Brenda <bee@bee.com>
2022-06-14 12:24:56 +0300

    Adds the different forms to be used

---
+++ awardsapp/forms.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import User
+from django.db.models import fields
+from .models import Ratings,Profile,Projects
+from django.forms import ModelForm
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+class SignupForm(UserCreationForm):
+  email=forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+  class Meta:
+    model = User
+    fields=['username','email','password1','password2']
+
+class AddProjectForm(ModelForm):
+  class Meta:
+    model = Projects
+    fields = ['name','description','project_image','urls']
+
+class RatingForm(ModelForm):
+  class Meta:
+    model = Ratings
+    fields = ['design','usability','content']
+
+class UpdateUserForm(ModelForm):
+  email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+  class Meta:
+    model = User
+    fields = ['username', 'email']
+
+class UpdateProfile(ModelForm):
+  class Meta:
+    model = Profile
+    fields = ['name','location','profile_pic','bio','email']
