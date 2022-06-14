b0e4906e3ae77f860f0195ac507232bd39b9f21d
Brenda <bee@bee.com>
2022-06-14 11:13:25 +0300

    Cteates the Profie Model

---
+++ awardsapp/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.db.models.signals import post_save
+from PIL import Image
+# Create your models here.
+
+class Profile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE, related_name='profile')
+    profile_pic=models.ImageField(upload_to='profile_pics')
+    bio=models.TextField()
+    location=models.CharField(max_length=50)
+    email=models.EmailField()
+    url=models.URLField()
+
+    def __str__(self):
+        return self.user.username
