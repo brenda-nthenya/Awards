3c0fe6a33cea0f600592f8d10f7c042b128161d0
Brenda <bee@bee.com>
2022-06-15 16:32:31 +0300

    Defines the projet view

--- awardsapp/views.py
+++ awardsapp/views.py
@@ -61,3 +61,49 @@ class ProjectsList(APIView):
         projects=Projects.objects.all()
         serializers=ProjectsSerializer(projects,many=True)
         return Response(serializers.data)
+
+
+@login_required(login_url='login')
+def project(request, project_id):
+    project = Projects.objects.get(pk=project_id)
+    ratings = Ratings.project_votes(project.id)
+    rating_status = None
+    if ratings is None:
+        rating_status = False
+    else:
+        rating_status = True
+    if request.method == 'POST':
+        form = RatingForm(request.POST)
+        if form.is_valid():
+            rate = form.save(commit=False)
+            rate.user = request.user
+            rate.project = project
+            rate.save()
+            post_ratings = Ratings.objects.filter(project=project)
+
+            design_ratings = [d.design for d in post_ratings]
+            design_average = sum(design_ratings) / len(design_ratings)
+
+            usability_ratings = [us.usability for us in post_ratings]
+            usability_average = sum(usability_ratings) / len(usability_ratings)
+
+            content_ratings = [content.content for content in post_ratings]
+            content_average = sum(content_ratings) / len(content_ratings)
+
+            score = (design_average + usability_average + content_average) / 3
+        
+            rate.design_average = round(design_average, 2)
+            rate.usability_average = round(usability_average, 2)
+            rate.content_average = round(content_average, 2)
+            rate.score = round(score, 2)
+            rate.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        form = RatingForm()
+    params = {
+        'project':project,
+        'rating_form': form,
+        'rating_status': rating_status
+
+    }
+    return render(request, 'project.html', params)
