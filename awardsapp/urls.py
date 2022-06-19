f3e0e1446d4d39d685a562520678b32623b02877
Brenda <bee@bee.com>
2022-06-19 14:18:51 +0300

    Adds the Projectlist and Profilelist API

--- awardsapp/urls.py
+++ awardsapp/urls.py
@@ -7,14 +7,16 @@ urlpatterns=[
     path('', views.index, name='index'),
     path('signup/',views.signup,name='signup'),
     path('project/<int:project_id>/',views.project,name='project'),
-    path('profile/<int:profile_id>/settings', views.edit_profile, name='edit'),
+    path('edit_profile/<int:profile_id>/', views.edit_profile, name='edit_profile'),
     path('add_project/',views.add_project,name='add_project'),
     path('rate_project/<int:project_id>/',views.rate_project,name = 'rate_project'),
-    path('api/profiles/',views.ProfileList.as_view()),
-    path('api/projects/',views.ProjectsList.as_view()),
     path('search_project/',views.search_project,name='search_project'),
     path('profile/<int:profile_id>/',views.profile,name='profile'),
-]
+    path('api/profiles/',views.ProfileList.as_view()),
+    path('api/projects/',views.ProjectsList.as_view()),
+    path('api/profile/',views.ProfilesList),
+    path('api/project/',views.ProjectList),
+   ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
